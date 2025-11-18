7c10f4d142839ed6786ab0a12954d25e9ca1630c
Fumiya Manno <fumiya@manno-cloud.com>
2025-11-18 21:53:39 +0900

    first commit

---
+++ appserver.tf
@@ -0,0 +1,64 @@
+#key pair
+resource "aws_key_pair" "keipair" {
+  key_name   = "${var.project}-${var.enviroment}-keipair"
+  public_key = file("./src/tastylog-dev-keypair.pub")
+
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-keypair"
+    project = var.project
+    env     = var.enviroment
+  }
+}
+
+#SSM Parameter Store
+resource "aws_ssm_parameter" "host" {
+  name  = "/${var.project}/${var.enviroment}/app/MYSQL_HOST"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.address
+}
+
+resource "aws_ssm_parameter" "port" {
+  name  = "/${var.project}/${var.enviroment}/app/MYSQL_PORT"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.port
+}
+
+resource "aws_ssm_parameter" "database" {
+  name  = "/${var.project}/${var.enviroment}/app/MYSQL_DATABASE"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.name
+}
+
+resource "aws_ssm_parameter" "username" {
+  name  = "/${var.project}/${var.enviroment}/app/MYSQL_USERNAME"
+  type  = "SecureString"
+  value = aws_db_instance.mysql_standalone.username
+}
+
+resource "aws_ssm_parameter" "password" {
+  name  = "/${var.project}/${var.enviroment}/app/MYSQL_PASSWORD"
+  type  = "SecureString"
+  value = aws_db_instance.mysql_standalone.password
+}
+
+
+#EC2 Instance
+resource "aws_instance" "app_server" {
+  ami                         = data.aws_ami.app.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public_subnet_1a.id
+  associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.app_ec2_profile.name
+  vpc_security_group_ids = [
+    aws_security_group.app_sg.id,
+    aws_security_group.opmng_sg.id
+  ]
+  key_name = aws_key_pair.keipair.key_name
+
+  tags = {
+    Name    = "${var.project}-${var.enviroment}-app-ec2"
+    project = var.project
+    env     = var.enviroment
+    Type    = "app"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "tastylog-tfstate-backet-fmanno"
+    key     = "tastylog-dev.tfstate"
+    region  = "ap-northeast-1"
+    profile = "terraform"
+  }
+}
+
+
+#Provider
+
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+#Variables
+
+variable "project" {
+  type = string
+}
+
+variable "enviroment" {
+  type = string
+}
+
