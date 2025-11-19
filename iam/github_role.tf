8e4499d63cd16475e8b1ac26598f09206bb04060
Fumiya Manno <fumiya@manno-cloud.com>
2025-11-19 23:51:27 +0900

    add iam

---
+++ iam/github_role.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "github_actions" {
+  name = "GitHubActionsTerraformRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:Manno-cloud/terraform:*"
+          }
+        }
+      }
+    ]
+  })
+}
