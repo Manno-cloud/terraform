7c28276b10749e10ab272eeb5ab52ae970ac9919
Fumiya Manno <fumiya@manno-cloud.com>
2025-11-20 01:27:59 +0900

    Add Terraform CI/CD workflow

--- iam/github_role.tf
+++ iam/github_role.tf
@@ -11,8 +11,11 @@ resource "aws_iam_role" "github_actions" {
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
+          },
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:Manno-cloud/terraform:*"
+            "token.actions.githubusercontent.com:sub" = "repo:Manno-cloud/terraform:pull_request"
           }
         }
       }
