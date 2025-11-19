4de6f47112b0c387333f3a1f29b2078d99d44eac
Fumiya Manno <fumiya@manno-cloud.com>
2025-11-19 20:45:01 +0900

    fix: update security group module

--- modules/security_group/security_group.tf
+++ modules/security_group/security_group.tf
@@ -15,31 +15,30 @@ resource "aws_security_group" "security_group" {
 resource "aws_security_group_rule" "ingress" {
   for_each = {
     for sg_name, sg in var.security_groups :
-    for rule in sg.ingress.rules :
-    "${sg_name}-${rule.from_port}-${rule.to_port}-${rule.protocol}" => {
+    sg_name => {
       sg_name = sg_name
-      rule    = rule
+      rules   = sg.ingress.rules
     }
   }
 
   type              = "ingress"
   security_group_id = aws_security_group.security_group[each.value.sg_name].id
+  from_port         = each.value.rules[0].from_port
+  to_port           = each.value.rules[0].to_port
+  protocol          = each.value.rules[0].protocol
 
-  from_port = each.value.rule.from_port
-  to_port   = each.value.rule.to_port
-  protocol  = each.value.rule.protocol
+  cidr_blocks = (
+    contains(keys(each.value.rules[0]), "cidr_blocks")
+    ? each.value.rules[0].cidr_blocks
+    : null
+  )
 
-  # --- CIDR がある場合 ---
-  cidr_blocks = try(each.value.rule.cidr_blocks, null)
-
-  # --- SG名 → SG ID に変換する処理（重要）---
   source_security_group_id = (
-    try(each.value.rule.source_sg, null) != null
-    ? aws_security_group.security_group[each.value.rule.source_sg].id
+    contains(keys(each.value.rules[0]), "source_sg")
+    ? each.value.rules[0].source_sg
     : null
   )
 }
-
 
 ###############################
 # Egress Rules
@@ -47,20 +46,28 @@ resource "aws_security_group_rule" "ingress" {
 resource "aws_security_group_rule" "egress" {
   for_each = {
     for sg_name, sg in var.security_groups :
-    for rule in sg.egress.rules :
-    "${sg_name}-${rule.from_port}-${rule.to_port}-${rule.protocol}" => {
+    sg_name => {
       sg_name = sg_name
-      rule    = rule
+      rules   = sg.egress.rules
     }
   }
 
   type              = "egress"
   security_group_id = aws_security_group.security_group[each.value.sg_name].id
+  from_port         = each.value.rules[0].from_port
+  to_port           = each.value.rules[0].to_port
+  protocol          = each.value.rules[0].protocol
 
-  from_port = each.value.rule.from_port
-  to_port   = each.value.rule.to_port
-  protocol  = each.value.rule.protocol
+  cidr_blocks = (
+    contains(keys(each.value.rules[0]), "cidr_blocks")
+    ? each.value.rules[0].cidr_blocks
+    : null
+  )
 
-  cidr_blocks = try(each.value.rule.cidr_blocks, null)
+  source_security_group_id = (
+    contains(keys(each.value.rules[0]), "source_sg")
+    ? each.value.rules[0].source_sg
+    : null
+  )
 }
 
