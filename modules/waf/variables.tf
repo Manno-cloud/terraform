c18201312270a4447e0f014934965239668bc6d7
Fumiya Manno <fumiya@manno-cloud.com>
2025-11-21 14:57:37 +0900

    test

---
+++ modules/waf/variables.tf
@@ -0,0 +1,12 @@
+variable "project" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "cloudfront_distribution_arn" {
+  type = string
+  description = "CloudFront Distribution ARN"
+}
