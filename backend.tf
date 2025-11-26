3071eaf94bc9984d2ed47986a83a78fd58936d41
Fumiya Manno <fumiya@manno-cloud.com>
2025-11-26 23:45:09 +0900

    chore: initial terraform infrastructure deployment

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tastylog-tfstate-backet-fmanno"
+    bucket         = "testapp-tfstate-backet-fmanno"
     key            = "main/terraform.tfstate"
     region         = "ap-northeast-1"
     dynamodb_table = "terraform-lock"
